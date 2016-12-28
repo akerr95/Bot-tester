7ef8879f9ec1704e1b65b4420a412fe324c483aa
Alec <akerr_95@hotmail.com>
2016-12-28 07:20:28 -0500

    Updated the server to work with Hapi, and also included the QNA bot service

--- messages/index.js
+++ messages/index.js
@@ -1,13 +1,80 @@
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 
 var useEmulator = (process.env.NODE_ENV === 'development');
 
-console.log(process.env.NODE_ENV);
+/**
+ * interface IWebResponse {
+ *     end(): this;
+ *     send(status: number, body?: any): this;
+ *     send(body: any): this;
+ *     status(code: number): this;
+ * }
+ */
+function responseWrapper(res) {
+    return {
+        end() {
+            res.end();
+            return this;
+        },
+
+        send(statusOrBody, maybeBody, callback) {
+            var responseCode = 200;
+            var responseBody = maybeBody;
+
+            if (typeof statusOrBody == 'number') {
+                responseCode = statusOrBody;
+                responseBody = maybeBody;
+            } else {
+                responseBody = statusOrBody;
+            }
+
+            if (typeof responseBody != 'string') {
+                responseBody = JSON.stringify(responseBody);
+            }
+
+            res.writeHead(responseCode);
+            res.setHeader("Content-Type", "application/json");
+            res.write(responseBody, callback);
+            res.end();
+            return this;
+        },
+
+        status(code) {
+            res.writeHead(code);
+            return this;
+        }
+    };
+}
+
+/**
+ * 
+ * interface IWebRequest {
+ *     body: any;
+ *     headers: {
+ *         [name: string]: string;
+ *     };
+ *     on(event: string, ...args: any[]): void;
+ * }
+ * 
+ */
+function requestWrapper(request) {
+    return {
+        body: request.payload,
+        headers: request.headers,
+        on(event, listener) {
+            request.raw.req.on(event, listener);
+            return this;
+        }
+    };
+}
+
+
 var connector = useEmulator ? new builder.ChatConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
+    appId: process.env.APPID,
+    appPassword: process.env.PASSWORD,
 }) : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
     appPassword: process.env['MicrosoftAppPassword'],
@@ -15,23 +82,70 @@ var connector = useEmulator ? new builder.ChatConnector({
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
+
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    session.send('Alec said ' + session.message.text);
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+    knowledgeBaseId:process.env.QnAKnowledgebaseId, 
+    subscriptionKey: process.env.QnASubscriptionKey
 });
+
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+                defaultMessage: 'No match! Try changing the query terms!',
+                qnaThreshold: 0.3}
+);
+
+bot.dialog('/', basicQnAMakerDialog);
 function status(request,reply){
-     connector.listen(request.raw.req,request.raw.res);
-     return reply("ok");
+      
+        var wrappedResponse = responseWrapper(request.raw.res);
+        var wrappedRequest = requestWrapper(request);
+        botconnect(wrappedRequest,wrappedResponse);
 }
+var botconnect = connector.listen();
 if (useEmulator) {
-     var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(8080, function() {
-        console.log('test bot endpont at http://localhost:8080/api/messages');
+  var Hapi = require("hapi");
+  var server =new Hapi.Server();
+  
+  server.connection({port:3987});
+  
+  server.route({
+    method: "POST",
+    path: '/api/messages',
+    handler:status
+});
+ server.register({
+    register: require("good"),
+    options: {
+        reporters: {
+            console: [{
+                module: "good-squeeze",
+                name: "Squeeze",
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: "good-console",
+            }, "stdout"]
+        }
+    }
+}, (err) => {
+    if (err) {
+        throw err;
+    }
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        server.log('info', 'Server running at: ' + server.info.uri);
     });
-    server.post('/api/messages', connector.listen());  
+});
+   
    
 } else {
-    module.exports = { default: connector.listen() }
+    module.exports = { default: connector.listen() };
 }
